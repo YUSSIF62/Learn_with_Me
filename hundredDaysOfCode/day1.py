95e52cbc844ae9102705c3a1429c94ee50a9c74f
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-02-21 12:47:32 +0000

    Update day1.py

--- hundredDaysOfCode/day1.py
+++ hundredDaysOfCode/day1.py
@@ -1,3 +1,4 @@
 print("Day One of Code")
 print('The course would take me 100 days to  complete the code challenge')
 #this is all about strings
+print("Hello world! \n Welcome to 100 days of code")

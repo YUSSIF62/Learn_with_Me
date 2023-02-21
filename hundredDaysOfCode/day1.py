c6f70cba2f3d38924f906bd96b20eb207e54dd8f
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-02-21 12:50:34 +0000

    Update day1.py

--- hundredDaysOfCode/day1.py
+++ hundredDaysOfCode/day1.py
@@ -2,3 +2,5 @@ print("Day One of Code")
 print('The course would take me 100 days to  complete the code challenge')
 #this is all about strings manipulation and code intelligence
 print("Hello world!\n Welcome to 100 days of code")
+name = 'Yussif'
+print("Hello " + name)

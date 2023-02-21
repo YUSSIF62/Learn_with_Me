de775f6922c168e8e703d2e77e68361143ec99fa
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-02-21 14:19:53 +0000

    Update day1.py

--- hundredDaysOfCode/day1.py
+++ hundredDaysOfCode/day1.py
@@ -4,3 +4,5 @@ print('The course would take me 100 days to  complete the code challenge')
 print("Hello world!\n Welcome to 100 days of code")
 name = 'Yussif'
 print("Hello " + name)
+#the Input function: this gives the user the chance to inputer enteries
+print('Hello ' +input("What is your name: "))

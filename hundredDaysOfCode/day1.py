4a3caa44ff9a688745aeae0980369e2831eba235
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-02-22 19:50:46 +0000

    Update day1.py

--- hundredDaysOfCode/day1.py
+++ hundredDaysOfCode/day1.py
@@ -1,11 +1,2 @@
-print("Day One of Code")
-print('The course would take me 100 days to  complete the code challenge')
-#this is all about strings manipulation and code intelligence
-print("Hello world!\n Welcome to 100 days of code")
-name = 'Yussif'
-print("Hello " + name)
-#the Input function: this gives the user the chance to inputer enteries
-print(len('Hello ' +input("What is your name: ")))
-input('What is your first name?  ')
-input('What is your surname?  ')
-print('you are welcome ')
+name = input('what is your name? ')
+print(len(name))

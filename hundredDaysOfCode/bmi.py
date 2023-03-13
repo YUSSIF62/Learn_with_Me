5bfb804c9744b75e01f406d0a9ae056e1f4fa49e
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-03-13 06:18:29 +0000

    Update bmi.py

--- hundredDaysOfCode/bmi.py
+++ hundredDaysOfCode/bmi.py
@@ -1,4 +1,7 @@
+w_as_int = weight
+h_as_int = height
 height= input("Enter height in m: ")
 weight = input("Enter weight in KG: ")
 bmi = weight / float(height) ** 2
+bmi_as_int= int(bmi)
 print(bmi)

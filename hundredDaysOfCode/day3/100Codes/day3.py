abc989d626e14f17ee302adb904795bfc8c04861
Abdul-Rahaman Yussif <abdulrahamanyussif62@gmail.com>
2023-03-13 06:52:58 +0000

    Update day3.py

--- hundredDaysOfCode/day3/100Codes/day3.py
+++ hundredDaysOfCode/day3/100Codes/day3.py
@@ -3,7 +3,7 @@ print("Welcome to Our platform")
 
 height = int(input("what is your height?  "))
 
-if height> 150:
+if height == 150:
     print("You are welcome once again ")
 else:
     print("Sorry, you are not welcome")
